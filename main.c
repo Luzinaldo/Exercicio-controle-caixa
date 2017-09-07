ca30e476e39289fb4e395a3daba6b8c254a4472d
matuidii <matheus.rios.oliveira@hotmail.com>
2017-09-07 02:06:30 -0300

    Criação da função cadastro

--- main.c
+++ main.c
@@ -26,9 +26,39 @@ typedef struct{
 }Movimentacao;
     Movimentacao movimentacao;
 
+    int contascadastradasG = 0;
+
 int main(){
+    int opcao;
+    setlocale(LC_ALL, "Portuguese");
+
+        do{
+            printf("\n\t -------------------------- Gerenciamento de Contas --------------------------\n");
+            printf("\n\t1 - Cadastro: ");
 
 
+            printf("\n\t4 - Sair");
+            printf("\n\t\t Escolha uma opção: ");
+            scanf("%d", &opcao);
 
+        }while(opcao =! 4);
     return 0;
+}
+
+void CadastroConta(){
+    int codigo;
+    char descricao;
+
+    printf(" Digite codigo da conta ");
+	scanf(" %d ", &codigo);
+	setbuf(stdin, NULL);
+
+	printf(" Digite descricao da conta ");
+	scanf(" %s ", &descricao);
+	setbuf(stdin, NULL);
+
+	conta[contascadastradasG].codigo = codigo;
+	strcpy(conta[contascadastradasG].descricao, descricao);
+
+	contascadastradasG++;
 }
