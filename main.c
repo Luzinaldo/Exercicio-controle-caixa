b71971159e307c901a32eb6763e29ecc74c1a637
matuidii <matheus.rios.oliveira@hotmail.com>
2017-09-07 02:56:24 -0300

    Finalizar funções, Acessar contas e Operações

--- main.c
+++ main.c
@@ -29,24 +29,31 @@ typedef struct{
     int contascadastradasG = 0;
 
 void CadastroConta();
+void AcessarConta();
 int main(){
-    char opcao;
+    int opcao;
     setlocale(LC_ALL, "Portuguese");
 
         do{
             printf("\n\t -------------------------- Gerenciamento de Contas --------------------------\n");
             printf("\n\t1 - Cadastro: ");
+            printf("\n\t2 - Acessar Conta: ");
+            printf("\n\t3 - Efetuar Operação");
             printf("\n\t4 - Sair");
             printf("\n\t\t Escolha uma opção: ");
-            scanf("%c", &opcao);
+            scanf("%d", &opcao);
             setbuf(stdin, NULL);
 
             switch(opcao){
 
-                case '1': CadastroConta(); break;
+                case 1: CadastroConta(); break;
+                case 2: AcessarConta(); break;
+                case 3: printf("teste3"); break;
+                case 4: printf("\n Saindo..."); break;
+                default: printf("\nOpçao inválida !! "); break;
             }
 
-        }while(opcao =! 4);
+        }while(opcao < 4);
     return 0;
 }
 
@@ -65,4 +72,34 @@ void CadastroConta(){
 	conta[contascadastradasG].codigo = codigo;
 	strcpy(conta[contascadastradasG].descricao, descricao);
 	contascadastradasG++; //Incremento de quantas contas serão cadastradas
+}
+
+void AcessarConta(){
+
+    int codigo, LINHA;
+    int pos = contascadastradasG;
+    printf("Digite o codigo da conta: ");
+    scanf("%d", &codigo);
+
+    for(LINHA = 0; LINHA < pos; LINHA++){
+        if(codigo == conta[LINHA].codigo){
+            //Chamar FUNÇÃO HISTORICO OU MOVIMENTAÇÃO
+            printf("\n\n JUVENAS");
+        }
+    }
+}
+
+void Operacao(){
+    char operacao;
+
+    printf("Tipo de operação: ");
+    printf("D - Débito || C - Credito: ");
+    scanf("%c", &operacao);
+
+
+    switch(operacao){
+
+        case '1':
+
+    }
 }
