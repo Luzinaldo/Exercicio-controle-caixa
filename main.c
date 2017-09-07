b85feed5d2cc6ddcb28513509c5c2dd424783341
matuidii <matheus.rios.oliveira@hotmail.com>
2017-09-07 01:55:13 -0300

    Adicionei structs e o define

--- main.c
+++ main.c
@@ -3,6 +3,28 @@
 #include <locale.h>
 #include <string.h>
 
+#define TAMCONTA 100
+
+typedef struct{
+    int codigo;
+    char descricao[100];
+}Conta;
+    Conta conta[TAMCONTA];
+
+typedef struct{
+    int codigo;
+    char descricao[100];
+    int tipo; //Credito ou Debito
+}Historico;
+    Historico historico;
+
+typedef struct{
+    int data; //dia, mes, ano
+    Conta conta;
+    Historico historico;
+    char complemento[100];
+}Movimentacao;
+    Movimentacao movimentacao;
 
 int main(){
 
